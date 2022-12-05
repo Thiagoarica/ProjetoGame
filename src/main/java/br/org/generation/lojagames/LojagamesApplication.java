025380ed5f1902d44b1553819b15ebb723ff93b8
Thiagoarica <thiago.arica@mercadolivre.com>
2022-12-05 12:54:36 -0300

    atualizado

--- src/main/java/br/org/generation/lojagames/LojagamesApplication.java
+++ src/main/java/br/org/generation/lojagames/LojagamesApplication.java
@@ -1,4 +1,4 @@
-package com.generation.lojagames;
+package br.org.generation.lojagames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LojagamesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LojagamesApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LojagamesApplication.class, args);
+	}
 
 }
--- src/test/java/br/org/generation/lojagames/LojagamesApplicationTests.java
+++ src/test/java/br/org/generation/lojagames/LojagamesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.generation.lojagames;
+package br.org.generation.lojagames;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LojagamesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
